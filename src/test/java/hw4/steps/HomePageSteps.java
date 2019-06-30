66b48a6264503bb5fb87e1750428723fc6d1bea2
Khodakovsky, Dmitry <Dmitrii.Khodakovsky@Clarivate.com>
2019-06-30 23:12:52 +0300

    HW4 is acceptedd

--- src/test/java/hw4/steps/HomePageSteps.java
+++ src/test/java/hw4/steps/HomePageSteps.java
@@ -7,6 +7,8 @@ public class HomePageSteps {
     HomePage homePage = new HomePage();
 
     public MetalsAndColorsPage openMetalsAndColorsPage() {
+        // TODO This step and class is redundant here
+        // TODO You could use this method in test directly from the selenide page object
         return homePage.openMetalsAndColorsPage();
     }
 
